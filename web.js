736220b53d42041ca83e67771e4564be978eb84e
Sean Chitwood <darkmane@gmail.com>
2015-11-27 10:09:06 -0800

    In progress

--- web.js
+++ web.js
@@ -10,51 +10,62 @@ var path = require('path');
 var cfg = require(path.join(__dirname, 'configuration', "config"));
 var app = express();
 var env = (process.env.NODE_ENV || 'DEVELOPMENT').toLowerCase();
-var Logger = require('winston');
+var winston = require('winston');
 var npid = require('npid');
 
 
-if (cluster.isMaster) {
-  Logger.log("Master is forking workers");
-  for (var i=0; i<numCPUs; ++i) {
-    cluster.fork();
-  }
-  return;
-}
+//if (cluster.isMaster) {
+//  Logger.log("Master is forking workers");
+//  for (var i=0; i<numCPUs; ++i) {
+//    cluster.fork();
+//  }
+//  return;
+//}
 
 // npid.create(path.join(__dirname, "pids", ("pid." + process.pid) ));
 npid.create(path.join(__dirname, "../shared/pids/node.pid"));
 
-Logger.log("Initiating worker, pid:" + process.pid);
-
-
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({ filename: 'somefile.log' })
+    ]
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+logger.log("Initiating worker, pid:" + process.pid);
+
+
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, '/views'));
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
+
+app.configure('development', function () {
+    app.use(express.errorHandler());
+});
+
+//app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
+app.use('/webcomponents', express.static('bower_components'));
 
 // app.get('/', routes.index);
-app.get('/', function(req, response) {
-		response.status(200).send(path.join(__dirname, 'public', 'index.html'));			
+app.get('/', function (req, response) {
+    response.status(200).send(path.join(__dirname, 'public', 'index.html'));
 });
 
 
-subcategory_ids = [  ];
+subcategory_ids = [];
 
 app.get('/ping', routes.ping);
 
-app.listen(app.get('port'), function(){
-  Logger.log("Express".green.bold + " server listening on port " + (app.get('port')+ "").green.bold);
+app.listen(app.get('port'), function () {
+    logger.log("Express".green.bold + " server listening on port " + (app.get('port') + "").green.bold);
 });
 
-Logger.log("Started.");
+logger.log("Started.");
